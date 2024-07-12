0debaf92bea2cb2914b32b608d646ff25b1ca2b0
Ido David Tavron <idodavid.tavron@live.biu.ac.il>
2024-07-12 11:18:47 +0300

    simple signal example

--- signal-store-app/src/app/app.component.ts
+++ signal-store-app/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal, computed, untracked, effect } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -9,5 +9,31 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'signal-store-app';
+  public basicSignal = signal(0);
+  public computerSignal = computed(() => this.basicSignal() * 10);
+  public untrackedSignal = computed(() => untracked(this.basicSignal) * 10);
+  public effectSignal = signal(0);
+  public message = signal('');
+  constructor() {
+    effect(() => {
+      const currentCounter = this.basicSignal();
+      this.message.update(message => `message ${currentCounter}`)
+    }, {
+      allowSignalWrites: true
+    });
+  
+  }
+
+  public setIncrement(): void {
+    this.basicSignal.set(this.basicSignal() + 1);
+  }
+
+  public updateIncrement(): void {
+    this.basicSignal.update(counter => counter + 1);
+  }
+
+  public effectTrigger(): void {
+    this.effectSignal.update(counter => counter + 1);
+  }
+  
 }
